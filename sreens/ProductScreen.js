b467f8403b111eee88d9aa15d813eab16173f1e3
Ivan <inzarubin80@gmail.com>
2020-08-29 15:16:33 +0300

    go from home

--- sreens/ProductScreen.js
+++ sreens/ProductScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-
+//1
 const ProductScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
